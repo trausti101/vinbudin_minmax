92ceb6879c969184f6fe38976baea8945f1341c3
trausti101 <trausti101@gmail.com>
2023-03-27 14:52:17 +0000

    Analysis done
    - Finished the analysis
    - Next step is to make this an app or worst case scenario a *n o t e b
      o o k* and publish it

--- test.R
+++ test.R
@@ -1,2 +1,24 @@
 library(httr)
 library(jsonlite)
+library(tidyverse)
+
+r <- GET(
+  "https://www.vinbudin.is/addons/origo/module/ajaxwebservices/search.asmx/DoSearch?&skip=0&count=10000000",
+  content_type_json()
+)
+
+data <- r$content %>%
+  rawToChar() %>%
+  fromJSON() %>%
+  paste() %>%
+  fromJSON(flatten = T)
+
+data <- data$data %>%
+  as_tibble()
+
+data <- data %>%
+  select(ProductName, ProductBottledVolume, ProductAlchoholVolume, ProductCategory.name, ProductPrice) %>%
+  rename("ProductCategory" = ProductCategory.name, "ProductAlcoholVolume" = ProductAlchoholVolume) %>%
+  mutate(ProductCategory = factor(ProductCategory), ProductAlcoholVolume = ProductAlcoholVolume/100) %>%
+  relocate(ProductName, ProductBottledVolume, ProductAlcoholVolume, ProductPrice, ProductCategory) %>%
+  mutate(AlcoholPricePer_mL = ProductPrice / (ProductBottledVolume * ProductAlcoholVolume))
